cd91b6014f6571bdefe823cd503d61a053168b5d
yuji <yuoda@yahoo.co.jp>
2021-03-27 12:58:49 +0900

    brush

--- src/script.py
+++ src/script.py
@@ -1,6 +1,6 @@
 
 from functools import reduce, wraps
-from typing import Callable, Any, Iterable, Union
+from typing import Callable, Any, Iterable, Union, TextIO
 
 
 def pipe(*functions: Callable[[Any], Any]):
@@ -15,23 +15,29 @@ print(pipe(lambda x: x*2, lambda x: x+2)(10))
 print(compose(lambda x: x*2, lambda x: x+2)(10))
 
 
-def from_file(func):
+def from_stream_supplier(func):
     @wraps(func)
-    def _wrapper(src: Union[str, Iterable[str]], *args, **kwargs):
+    def _wrapper(src: Union[str, Iterable[str], Callable[[], Union[TextIO, Iterable[str]]]], *args, **kwargs):
         if isinstance(src, str):
             open_args = {'file': src, 'encoding': kwargs.pop('encoding') if 'encoding' in kwargs else 'utf-8'}
             with open(**open_args) as fin:
                 return func(fin, *args, **kwargs)
-        else:
+        elif callable(src):
+            with src() as fin:
+                func(fin, *args, **kwargs)
+        elif hasattr(src, '__iter__'):
             return func(src, *args, **kwargs)
+        else:
+            print('src not supported')
     return _wrapper
 
 
-@from_file
-def print_lines(stream: Iterable[str]):
+@from_stream_supplier
+def print_elements(stream: Union[Iterable[str], Callable[[], Union[TextIO, Iterable[str]]]]):
     for line in stream:
         print(line.rstrip())
 
 
-print_lines('logger.py', encoding='utf-8')
-print_lines(iter('logger.py'))
+print_elements('logger.py', encoding='utf-8')
+print_elements(iter('logger.py'))
+print_elements(lambda: open('logger_injector.py'))
