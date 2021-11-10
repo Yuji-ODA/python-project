9e29d1bc2dc0708612175474ead349a7cc2a9674
yuji <yuoda@yahoo.co.jp>
2021-11-10 14:33:24 +0900

    modify sampling_test.py

--- src/sampling_test_driver.py
+++ src/sampling_test_driver.py
@@ -2,8 +2,8 @@ from random import sample
 
 import numpy as np
 
-set1 = set(range(0, 100000))
-set2 = set(range(50000, 150000))
+set1 = set(range(0, 250000))
+set2 = set(range(200000, 500000))
 
 intersect_len = len(set(set1).intersection(set2))
 union_len = len(set(set1).union(set2))
@@ -21,9 +21,9 @@ for sampling_rate in np.arange(0.1, 1, 0.1):
     sample1 = set(sample(set1, int(len(set1) * sampling_rate)))
     sample2 = set(sample(set2, int(len(set2) * sampling_rate)))
 
-    sample_inter_size = len(sample1.intersection(sample2))
+    sample_intersection_size = len(sample1.intersection(sample2))
     sample_union_size = len(sample1.union(sample2))
 
     print(f'expected: {p12 * sample_union_size}, proportion: {p12}')
-    print(f'actual: {sample_inter_size}, proportion: {sample_inter_size / sample_union_size}')
+    print(f'actual: {sample_intersection_size}, proportion: {sample_intersection_size / sample_union_size}')
     print(f'estimated: {p12 * sample_union_size * sampling_rate}, proportion: {p12 * sampling_rate}')
