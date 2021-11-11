2e88988f8856eb903fe44bfa5eae18ab984f4fd6
yuji <yuoda@yahoo.co.jp>
2021-11-11 10:40:06 +0900

    fix

--- src/sampling_simulator.py
+++ src/sampling_simulator.py
@@ -9,18 +9,20 @@ def main(population1, population2):
     size2 = len(population2)
 
     intersect = set(population1).intersection(population2)
-    intersect_size = len(intersect)
     union = set(population1).union(population2)
-    union_size = len(union)
 
-    p1 = (size1 - intersect_size) / union_size
-    p2 = (size2 - intersect_size) / union_size
-    p12 = intersect_size / union_size
+    n_all = len(union)
+    n12 = len(intersect)
+    p12 = n12 / n_all
+    n1 = size1 - n12
+    n2 = size2 - n12
+    p1 = n1 / n_all
+    p2 = n2 / n_all
 
-    print(f'p1: {p1}, p2: {p2}, p12: {p12}')
+    print(f'p12: {p12}, p1: {p1}, p2: {p2}')
 
     for sampling_rate in np.arange(0.1, 1, 0.1):
-        print('==============================================================')
+        print('=======================================================================================')
         print(f'sampling rate: {sampling_rate}')
 
         # 個別サンプリング
@@ -28,33 +30,42 @@ def main(population1, population2):
         sample2 = set(sample(list(population2), int(size2 * sampling_rate)))
 
         sample_intersection = sample1.intersection(sample2)
-        sample_union_size_actual = len(sample1.union(sample2))
+        n_all_actual = len(sample1.union(sample2))
         n12_actual = len(sample_intersection)
-        p12_actual = n12_actual / sample_union_size_actual
+        n1_actual = len(sample1) - n12_actual
+        n2_actual = len(sample2) - n12_actual
+
+        p12_actual, p1_actual, p2_actual = np.array((n12_actual, n1_actual, n2_actual)) / n_all_actual
 
         # 理論値の計算
-        n12_expected = sampling_rate * intersect_size
-        p12_expected = p12
+        n12_expected, n1_expected, n2_expected = sampling_rate * np.array((n12, n1, n2))
+        p12_expected, p1_expected, p2_expected = p12, p1, p2
 
         # 個別サンプリングの場合の理論値の計算
         # 各サンプリングで選ばれる確率はsampling_rateに等しいので重複する確率はsampling_rateの二乗となる
         # これに母集合の重複数をかけて重複数の期待値を得る
-        n12_computed = (sampling_rate ** 2) * intersect_size
+        n12_computed = (sampling_rate ** 2) * n12
         # サンプリングの総数から重複分を引く
-        sample_union_size_computed = sampling_rate * (size1 + size2) - n12_computed
-        p12_computed = n12_computed / sample_union_size_computed
+        n1_computed, n2_computed = np.array((size1, size2)) * sampling_rate - n12_computed
+        n_all_computed = n1_computed + n2_computed + n12_computed
+        p12_computed, p1_computed, p2_computed = np.array((n12_computed, n1_computed, n2_computed)) / n_all_computed
 
         # 補正
         # 重複分の取りこぼしを補正
         n12_corrected = n12_actual / sampling_rate
-        # 重複分を増やした分だけ全体数を減らす
-        sample_union_size_corrected = sample_union_size_actual - (n12_corrected - n12_actual)
-        p12_corrected = n12_corrected / sample_union_size_corrected
+        # 重複分を増やした分だけ減らす
+        n_all_corrected, n1_corrected, n2_corrected = np.array((n_all_actual, n1_actual, n2_actual)) - (n12_corrected - n12_actual)
+        p12_corrected, p1_corrected, p2_corrected = np.array((n12_corrected, n1_corrected, n2_corrected)) / n_all_corrected
 
-        print(f'expected: {n12_expected}, proportion: {p12_expected}')
-        print(f'actual: {n12_actual}, proportion: {p12_actual}')
-        print(f'computed: {n12_computed}, proportion: {p12_computed}')
-        print(f'corrected: {n12_corrected}, proportion: {p12_corrected}')
+        print_result('expected ', n12_expected, n1_expected, n2_expected, p12_expected, p1_expected, p2_expected)
+        print_result('actual   ', n12_actual, n1_actual, n2_actual, p12_actual, p1_actual, p2_actual)
+        print_result('computed ', n12_computed, n1_computed, n2_computed, p12_computed, p1_computed, p2_computed)
+        print_result('corrected', n12_corrected, n1_corrected, n2_corrected, p12_corrected, p1_corrected, p2_corrected)
+
+
+def print_result(header, n12, n1, n2, p12, p1, p2):
+    print(f'{header}: n12 = {round(n12)}, n1 = {round(n1)}, n2 = {round(n2)}, '
+          f'p12 = {p12:.4f}, p1 = {p1:.4f}, p2 = {p2:.4f}')
 
 
 if __name__ == '__main__':
