ed3cb29fd1fb9f0b0e13519e0d3168001a30f110
yuji <yuoda@yahoo.co.jp>
2021-11-12 16:09:49 +0900

    add Cardinality3

--- src/sampling_simulator_util.py
+++ src/sampling_simulator_util.py
@@ -15,6 +15,17 @@ class Cardinality2:
         return np.array((self.p1, self.p12, self.p2))
 
 
+class Cardinality3:
+    def __init__(self, v1: float, v2: float, v3: float, v12: float, v13: float, v23: float, v123: float):
+        args = (v1, v2, v3, v12, v13, v23, v123)
+        self.v1, self.v2, self.v3, self.v12, self.v13, self.v23, self.v123 = args
+        self.total_count = sum(args)
+        self.p1, self.p2, self.p3, self.p12, self.p13, self.p23, self.p123 = [v / self.total_count for v in args]
+
+    def normalize(self):
+        return np.array((self.p1, self.p2, self.p3, self.p12, self.p13, self.p23, self.p123))
+
+
 def decompose2(population1, population2) -> Cardinality2:
     intersect = set(population1).intersection(population2)
 
