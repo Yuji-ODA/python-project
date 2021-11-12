f7719c7ff83b98d97a27acbb751a29327ec23f71
yuji <yuoda@yahoo.co.jp>
2021-11-12 14:23:17 +0900

    fix

--- src/sampling_simulator.py
+++ src/sampling_simulator.py
@@ -6,41 +6,41 @@ from numpy.linalg import norm
 from src.sampling_simulator_util import array, decompose2, Stats2
 
 
-def simulate(population1, population2, n_stats, p_stats, sampling_rate1, sampling_rate2=None):
+def simulate(population1, population2, n, p, sampling_rate1, sampling_rate2=None):
 
     if sampling_rate2 is None:
         sampling_rate2 = sampling_rate1
 
     # 理論値の計算
     # 抽出率が違う場合は考えられないので、ここでは小さい方の値である場合を想定する
-    n1_expected, n12_expected, n2_expected = min(sampling_rate1, sampling_rate2) * array(n_stats.v1, n_stats.v12, n_stats.v2)
+    n1_expected, n12_expected, n2_expected = min(sampling_rate1, sampling_rate2) * array(n.v1, n.v12, n.v2)
     n_stats_expected = Stats2(n1_expected, n12_expected, n2_expected)
-    p_stats_expected = p_stats
+    p_stats_expected = p
 
     # 個別サンプリングの場合の理論値の計算
-    n_stats_computed, p_stats_computed = do_estimation(sampling_rate1, sampling_rate2, n_stats)
+    n_computed, p_computed = do_estimation(sampling_rate1, sampling_rate2, n)
 
     # 個別サンプリングの結果を取得する
-    n_stats_actual, p_stats_actual = do_sampling(population1, population2, sampling_rate1, sampling_rate2)
+    n_actual, p_actual = do_sampling(population1, population2, sampling_rate1, sampling_rate2)
 
     # 補正計算
-    n_stats_corrected, p_stats_corrected = do_correction(n_stats_actual, sampling_rate1, sampling_rate2)
+    n_corrected, p_corrected = do_correction(n_actual, sampling_rate1, sampling_rate2)
 
     # 誤差計算
-    probs_expected = array(p_stats_expected.v1, p_stats_expected.v12, p_stats_expected.v2)
-    err_actual = norm(array(p_stats_actual.v1, p_stats_actual.v12, p_stats_actual.v2) - probs_expected)
-    err_computed = norm(array(p_stats_computed.v1, p_stats_computed.v12, p_stats_computed.v2) - probs_expected)
-    err_corrected = norm(array(p_stats_corrected.v1, p_stats_corrected.v12, p_stats_corrected.v2) - probs_expected)
+    probabilities_expected = array(p_stats_expected.v1, p_stats_expected.v12, p_stats_expected.v2)
+    err_actual = norm(array(p_actual.v1, p_actual.v12, p_actual.v2) - probabilities_expected)
+    err_computed = norm(array(p_computed.v1, p_computed.v12, p_computed.v2) - probabilities_expected)
+    err_corrected = norm(array(p_corrected.v1, p_corrected.v12, p_corrected.v2) - probabilities_expected)
 
     print_result('expected ', n_stats_expected, p_stats_expected)
-    print_result('actual   ', n_stats_actual, p_stats_actual, err_actual)
-    print_result('computed ', n_stats_computed, p_stats_computed, err_computed)
-    print_result('corrected', n_stats_corrected, p_stats_corrected, err_corrected)
+    print_result('actual   ', n_actual, p_actual, err_actual)
+    print_result('computed ', n_computed, p_computed, err_computed)
+    print_result('corrected', n_corrected, p_corrected, err_corrected)
 
 
-def print_result(header, n_stats, p_stats, err=math.nan):
-    print(f'{header}: n1 = {round(n_stats.v1)}, n12 = {round(n_stats.v1)}, n2 = {round(n_stats.v2)}, '
-          f'p1 = {p_stats.v1:.4f}, p12 = {p_stats.v12:.4f}, p2 = {p_stats.v2:.4f}, err = {err:.6f}')
+def print_result(header, n, p, err=math.nan):
+    print(f'{header}: n1 = {round(n.v1)}, n12 = {round(n.v1)}, n2 = {round(n.v2)}, '
+          f'p1 = {p.v1:.4f}, p12 = {p.v12:.4f}, p2 = {p.v2:.4f}, err = {err:.6f}')
 
 
 # 個別サンプリング
@@ -52,17 +52,17 @@ def do_sampling(population1, population2, sampling_rate1, sampling_rate2):
 
 
 # 補正計算
-def do_correction(n_stats_actual, sampling_rate1, sampling_rate2):
+def do_correction(n_actual, sampling_rate1, sampling_rate2):
 
     # 抽出率は低いほうに合わせてサイズを落とし想定に合わせる
     sampling_rate = min(sampling_rate1, sampling_rate2)
 
     # 重複分の取りこぼしを補正
-    n12_corrected = n_stats_actual.v12 / max(sampling_rate1, sampling_rate2)
+    n12_corrected = n_actual.v12 / max(sampling_rate1, sampling_rate2)
 
-    # 重複分を増やした分だけ減らす
-    n1_corrected = sampling_rate / sampling_rate1 * (n_stats_actual.v1 - (n12_corrected - n_stats_actual.v12))
-    n2_corrected = sampling_rate / sampling_rate2 * (n_stats_actual.v2 - (n12_corrected - n_stats_actual.v12))
+    # 各サンプルサイズをスケーリングしたのちに重複分を増やした分だけ引く
+    n1_corrected = sampling_rate / sampling_rate1 * (n_actual.v1 + n_actual.v12) - n12_corrected
+    n2_corrected = sampling_rate / sampling_rate2 * (n_actual.v12 + n_actual.v2) - n12_corrected
     n_all_corrected = n1_corrected + n12_corrected + n2_corrected
     p1_corrected, p12_corrected, p2_corrected = array(n1_corrected, n12_corrected, n2_corrected) / n_all_corrected
 
@@ -70,13 +70,13 @@ def do_correction(n_stats_actual, sampling_rate1, sampling_rate2):
 
 
 # 個別サンプリングの場合の理論値の計算
-def do_estimation(sampling_rate1, sampling_rate2, n_stats):
+def do_estimation(sampling_rate1, sampling_rate2, n):
     # 各サンプリングで選ばれる確率はsampling_rateに等しいので重複する確率はsampling_rateの積となる
     # これに母集合の重複数をかけて重複数の期待値を得る
-    n12_computed = sampling_rate1 * sampling_rate2 * n_stats.v12
+    n12_computed = sampling_rate1 * sampling_rate2 * n.v12
     # サンプリングの総数から重複分を引く
-    size1 = n_stats.v1 + n_stats.v12
-    size2 = n_stats.v12 + n_stats.v2
+    size1 = n.v1 + n.v12
+    size2 = n.v12 + n.v2
     n1_computed, n2_computed = array(sampling_rate1, sampling_rate2) * array(size1, size2) - n12_computed
     n_all_computed = n1_computed + n2_computed + n12_computed
     p12_computed, p1_computed, p2_computed = array(n12_computed, n1_computed, n2_computed) / n_all_computed
