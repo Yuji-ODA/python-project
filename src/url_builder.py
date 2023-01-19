4732d5bffbf1dab226152f49847c6bc35de29f20
yuji <ugYBAd0leb>
2023-01-19 14:13:08 +0900

    brush

--- src/url_builder.py
+++ src/url_builder.py
@@ -1,39 +1,42 @@
 from functools import reduce
 from itertools import chain
 from typing import List, TypeVar, Callable, Iterator, MutableMapping
-from urllib.parse import urlparse, ParseResult, urlunparse, urlencode, parse_qs
+from urllib.parse import SplitResult, urlencode, urlsplit, parse_qs
 
 T = TypeVar('T')
 QueryMapping = MutableMapping[str, List[str]]
 
 
 class UrlBuilder:
-    def __init__(self, parse_result: ParseResult):
-        self._p = parse_result
+    def __init__(self, split_result: SplitResult):
+        self._p = split_result
 
     @classmethod
     def from_url_string(cls, url_str: str):
-        return cls(urlparse(url_str))
+        return cls(urlsplit(url_str))
 
     @classmethod
-    def from_parse_result(cls, parse_result: ParseResult):
-        return cls(parse_result)
+    def from_split_result(cls, split_result: SplitResult):
+        return cls(split_result)
 
     def path_segment(self, *path: str) -> 'UrlBuilder':
         new_path = reduce(lambda acc, e: f'{acc}/{e}', path, self._p.path.rstrip('/'))
-        return self.from_parse_result(self._p._replace(path=new_path))
+        return self.from_split_result(self._p._replace(path=new_path))
 
     def query_param(self, name: str, *values: str) -> 'UrlBuilder':
         query_mapping = parse_qs(self._p.query)
         add_values(query_mapping, name, *values)
         queries = expand_mapping(lambda n, v: f'{n}={v}', query_mapping)
         new_query = '&'.join(queries)
-        return self.from_parse_result(self._p._replace(query=new_query))
+        return self.from_split_result(self._p._replace(query=new_query))
+
+    def fragment(self, fragment: str):
+        return self.from_split_result(self._p._replace(fragment=fragment))
 
     def build(self) -> str:
         query_mapping = parse_qs(self._p.query)
         encoded_query = urlencode(list(expand_mapping(lambda n, v: (n, v), query_mapping)))
-        return urlunparse(self._p._replace(query=encoded_query))
+        return self._p._replace(query=encoded_query).geturl()
 
 
 def expand_mapping(f: Callable[[str, str], T], mapping: QueryMapping) -> Iterator[T]:
@@ -54,9 +57,9 @@ if __name__ == '__main__':
 
     url = UrlBuilder.from_url_string('https://www.yahoo.co.jp/')\
         .query_param('key1', 'key1-value1', 'key1-value2')\
-        .query_param('key2', 'key2-value1', 'key2-value2')\
+        .query_param('key2', 'key2-value1', 'key2-value2', '日本語')\
         .path_segment('l1', 'l2', 'l3')\
+        .fragment('f1')\
         .build()
 
     print(url)
-
