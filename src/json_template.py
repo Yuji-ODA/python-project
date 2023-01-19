cd6b291c41b329ba532e6a1beb8c56d873722234
yuji <ugYBAd0leb>
2023-01-19 16:32:19 +0900

    add JsonMappingClass

--- src/json_template.py
+++ src/json_template.py
@@ -1,32 +1,67 @@
-
 import json
 from string import Template
-from typing import Dict, Union, List, Mapping, Callable
+from typing import Dict, Union, List, Mapping, Callable, Iterator
 
 JsonValue = Union[str, int, float, List['JsonValue'], Dict[str, 'JsonValue']]
 JsonType = Union[List[JsonValue], Dict[str, JsonValue]]
 JsonMapping = Mapping[str, JsonValue]
 
 
-def load_template(template_file: str, encoding: str = 'utf-8') -> Callable[[JsonMapping], JsonType]:
+class JsonMappingClass(Mapping[str, JsonValue]):
+
+    def __getitem__(self, key: str) -> JsonValue:
+        return self.__dict__.__getitem__(key)
+
+    def __len__(self) -> int:
+        return self.__dict__.__len__()
+
+    def __iter__(self) -> Iterator[str]:
+        return self.__dict__.__iter__()
+
+    def items(self):
+        return self.__dict__.items()
+
+    def apply_template(self, template_file: str, encoding='utf-8') -> JsonType:
+        return load_template(template_file, encoding=encoding)(self)
+
+
+def load_template(template_file: str, encoding: str = 'utf-8') -> Callable[[JsonMapping | JsonMappingClass], JsonType]:
     with open(template_file, encoding=encoding) as f:
         t = Template(f.read())
+
     return lambda mapping: json.loads(
         t.safe_substitute({k: json.dumps(v) for k, v in mapping.items()}))
-
-
-def load_json(template_file: str, mapping: JsonMapping) -> JsonType:
-    return load_template(template_file)(mapping)
 
 
 if __name__ == '__main__':
     json_loader = load_template('template/template.json.tpl')
 
     params = {
-        'int': 1.2,
-        'str': '文字列',
-        'dict': {'key': '値', 'list': [3, 4, 5]},
-        'list': [1, 2, 3, {'key': 'value'}],
+        'int_val': 1,
+        'float_val': .2,
+        'str_val': '文字列',
+        'dict_val': {'key': '値', 'list': [3, 4, 5]},
+        'list_val': [1, 2, 3, {'key': 'value'}],
     }
 
     print(json_loader(params))
+
+    import dataclasses
+
+    @dataclasses.dataclass(frozen=True)
+    class ExampleParams(JsonMappingClass):
+        int_val: int
+        float_val: float
+        str_val: str
+        dict_val: Dict[str, JsonValue]
+        list_val: List[JsonValue]
+
+    params = ExampleParams(
+        int_val=1,
+        float_val=.3,
+        str_val='文字列',
+        dict_val={'key': '値', 'list': [3, 4, 5]},
+        list_val=[1, 2, 3, {'key': 'value'}]
+    )
+
+    print(params.apply_template('template/template.json.tpl'))
--- src/template/template.json.tpl
+++ src/template/template.json.tpl
@@ -1,6 +1,7 @@
 {
-  "int": ${int},
-  "str": ${str},
-  "dict": ${dict},
-  "list": ${list}
+  "int": ${int_val},
+  "float": ${float_val},
+  "str": ${str_val},
+  "dict": ${dict_val},
+  "list": ${list_val}
 }
