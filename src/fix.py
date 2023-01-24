543290820ba65e04698c1391f524e623503a6d6c
yuji <ugYBAd0leb>
2023-01-24 13:19:30 +0900

    brush

--- src/fix.py
+++ src/fix.py
@@ -2,18 +2,22 @@ from functools import partial
 from inspect import signature
 
 
-def y_combinator(f): return lambda x: f(y_combinator(x(x)))
+def Y(f):
+    return (lambda x: f(x(x)))(lambda x: f(x(x)))
 
 
-def z(f): return (lambda x: f(lambda y: x(x)(y)))(lambda x: f(lambda y: x(x)(y)))
+def Z(f):
+    return (lambda x: f(lambda *y: x(x)(*y)))(lambda x: f(lambda *y: x(x)(*y)))
 
 
-def fix(f): return f(lambda x: fix(f)(x))
+def fix(f):
+    return f(lambda x: fix(f)(x))
 
 
-def curried(f): return f if len(signature(f).parameters) == 1 else lambda x: curried(partial(f, x))
+def curried(f):
+    return f if len(signature(f).parameters) == 1 else lambda x: curried(partial(f, x))
 
 
 if __name__ == '__main__':
-    fib_40 = fix(lambda f: curried(lambda a0, a1, n: a0 if n <= 0 else f(a1)(a0+a1)(n-1)))(0)(1)(40)
+    fib_40 = Z(lambda f: curried(lambda a0, a1, n: a0 if n <= 0 else f(a1)(a0+a1)(n-1)))(0)(1)(40)
     print(fib_40)
