e2e81db4fe2832c17c96169b4f955f84c4557c9e
yuji <ugYBAd0leb>
2023-09-21 10:49:37 +0900

    brush

--- src/random_string.py
+++ src/random_string.py
@@ -1,19 +1,20 @@
 #!/usr/bin/env python3
 
 import random
+import string
 
-NUMBER = tuple(str(i) for i in range(10))
-ALPHAMERIC = tuple(list(NUMBER) + [chr(ord('A') + i) for i in range(26)] + [chr(ord('a') + i) for i in range(26)] + ['-'])
-ALPHAMERIC_AND_SYMBOL = tuple(ALPHAMERIC + ('$', '#', '=', '?', '_', '[', ']', '/'))
+NUMBERS = string.digits
+ALPHAMERIC = NUMBERS + string.ascii_letters
+ALPHAMERIC_AND_SYMBOLS = ALPHAMERIC + '-$#=?_[]/'
 
 
 def random_string(length, letters=ALPHAMERIC):
-  return ''.join([letters[random.randint(0, len(letters) - 1)] for _ in range(length)])
+    return ''.join([letters[random.randint(0, len(letters) - 1)] for _ in range(length)])
 
 
 if __name__ == '__main__':
     import sys
 
-    length = nt(sys.argv[1]) if 1 < len(sys.argv) else 10
+    length = int(sys.argv[1]) if 1 < len(sys.argv) else 16
 
     print(random_string(length, ALPHAMERIC))
